36cb0cd89e55aa6569685034fcb67ce2919acb2b
Tanvir3744 <biplobmohammad1@gmail.com>
2023-04-03 02:28:29 +0600

    varibles and data types

--- firstCProgramming/dataType.c
+++ firstCProgramming/dataType.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
+#include <math.h> // this header is used for mathmatical operation
 int main(){
-    printf("hello world ");
+    int rahim = 10;
+    int karim = 12;
+    float mohammad = 3.4;
+    printf("%d%d", karim, rahim);
     return 0;
 }
 
-/* 
-notes for data type : 
-
+/*
+notes for data type :
+ 1. there are four kind of data types in c programming
+ such as, integer, float, character, boolean,
+ 2. if we want to print an integer we should use foramte specifier like %d for integer,
+    %f -> fload, %c -> for character printing;
+ 3. to make some mathmatical operation should use math.h header 
+ 4. 
 
  */
