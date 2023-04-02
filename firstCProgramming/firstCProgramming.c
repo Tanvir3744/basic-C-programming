7e91c53ecdc1a28937fa6d095e411d8b24da309b
Tanvir3744 <biplobmohammad1@gmail.com>
2023-04-03 02:01:17 +0600

    datatypes and variables in c

---
+++ firstCProgramming/firstCProgramming.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    printf("hello world \n");
+    printf("hello programmers");
+    return 0;
+}
+// this is a comment in c and essential codes .
+/* 
+notes: 
+1. decalre new line while we want to see our output from printF printf("hello world")
+2. if we want to print a \ backsalsh we can simply put dual backslash here 
+3. c programming could not able to print % sign
+4. \n for new line
+5. \t for tab (4 space command)
+6.
+ */
