f79933047c6d62c24c9440c3818c7cfee0c83917
Tanvir3744 <biplobmohammad1@gmail.com>
2023-04-03 21:27:02 +0600

    operators in c

---
+++ operators/operators.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(){
+    printf("hello welcome to operators in c");
+    return 0;
+}
+
+/*
+note: here are some basic usage of operators and and conditional statements in c
+  1 -> + (addition) to add to numbers
+  2 -> - (substraction) to substract between two numbers
+  3 -> / (division) to divide to number
+  4 -> * (multiplication) to multiply numbers
+  5 -> % (modulas) to calculate reminders
+  logical operators
+  && (and) operator -> check both statement is true or note if any single value is false returns false
+  || (or) operator -> check any one value is true or note if any value is  returning true returns true;
+  > ( greater than) -> compare between two statement
+  < (less than ) -> compare between two statement
+  >= (greater than or equal ) compare statements greater than or equal 
+  <= (less than or equal ) compare statement less than or equal or not
+  == (double equal ) check the statements are equal or not 
+  === ( triple equal) check the satements are equal or not also with datatypes
+  
+ */
