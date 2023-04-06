84105a703f47bec437447d57d11d7058a33f8929
Tanvir3744 <biplobmohammad1@gmail.com>
2023-04-07 00:44:57 +0600

    practiced some basic problems

---
+++ practice/n_number.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    int n;
+    scanf("%d", &n);
+
+    int i;
+    for (i =1; i <= n; i++){
+        printf("I Love Practice\n");
+    }
+    return 0;
+}
---
+++ practice/variable_problem.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+ int main() {
+
+    int A;
+    long long int B;
+    float C;
+    char D;
+    scanf("%d %lld %f %c\n", &A,&B,&C,&D);
+    printf("%d\n%lld\n%0.2f\n%c\n", A, B, C, D);
+    return 0;
+} 
+/* 
+int main() {
+    int a;
+    long int b;
+    float c;
+    char d;
+    scanf("%d %ld %f %c", &a, &b, &c, &d);
+    printf("%d %ld %.2f %c\n", a, b, c, d);
+    return 0;
+} */
+
+
+
+
+
