5186b730cb9a62e0817e8e091ffa8bdf14fe6d2f
Tanvir3744 <biplobmohammad1@gmail.com>
2023-04-11 23:07:54 +0600

    worked with array in c

---
+++ array/arr.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int ar[10];
+    for (int i = 0; i < 10;i++)
+    {
+        printf("%d\n", i);
+    }
+
+    return 0;
+}
---
+++ array/inputOfArray.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    int arr[10];
+    for (int i = 0; i < 10; i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for (int i = 0; i < 10; i++)
+    {
+        printf("%d\n", arr[i]);
+    }
+    return 0;
+}
